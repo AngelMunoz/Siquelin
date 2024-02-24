4e173123a522a64ea32c94813e47247aabf6184b
Angel D. Munoz <scythes.lol@gmail.com>
2024-02-23 22:04:36 -0600

    add DI related types

--- Types.fs
+++ Types.fs
@@ -21,8 +21,26 @@ module Env =
     databasePath: string
   }
 
+  [<Struct>]
+  type ShiftItemQueryError = | WorkDayNotFound
+
+  type WorkdayService =
+    abstract member create: DateOnly -> unit
+    abstract member list: unit -> WorkDay list
+    abstract member get: DateOnly -> WorkDay option
+    abstract member exists: DateOnly -> bool
+
+  type ShiftItemService =
+    abstract member create:
+      DateOnly * TimeOnly * TimeOnly -> Result<unit, ShiftItemQueryError>
+
+    abstract member list:
+      DateOnly -> Result<ShiftItem list, ShiftItemQueryError>
+
   type AppEnv = {
     locations: SiquelinDataLocations
     logger: ILogger
     migrondi: IMigrondi
+    workdays: WorkdayService
+    shiftItems: ShiftItemService
   }
